b7c85fafbe5c073eacf672e85ecf143149c1f9e5
xw <1938690786@qq.com>
2023-07-24 18:24:00 +0800

    config

--- .dumirc.ts
+++ .dumirc.ts
@@ -4,11 +4,6 @@ const repo = 'my-blogs-docs'; // 项目名(也就是你的仓库名)
 
 export default defineConfig({
   title: "xw's docs",
-  mode: 'site',
-  // more config: https://d.umijs.org/config
-  devServer: {
-    port: 1998, // 自定义端口号
-  },
   base: `/${repo}/`,
   publicPath: `/${repo}/`,
 });
